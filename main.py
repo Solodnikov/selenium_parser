b05418c55a692c40566fd28880a862efc4a90a6b
Solodnikov <solodnikov@yandex.ru>
2023-10-03 18:31:29 +0300

    dotenv add

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+from dotenv import load_dotenv
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from fake_useragent import UserAgent
@@ -10,10 +12,11 @@ from functions import (authorization_hh, # noqa
                        get_vacancy_info,
                        collecting_test_info)
 
+load_dotenv()
 
 useragent = UserAgent()
-email = 'alexander.solodnikov@gmail.com'
-password = 'Karabas19!'
+email = os.getenv('EMAIL')
+password = os.getenv('PASSWORD')
 
 # options
 options = webdriver.ChromeOptions()
@@ -24,10 +27,14 @@ options.add_argument('--disable-blink-features=AutomationControlled')
 # headless mode
 # options.add_argument('--headless')
 
-# driver.get('https://www.whatismybrowser.com/detect/what-is-my-user-agent/',
-# driver_path
+# путь веб драйвера для пк
+# service = Service(
+#     executable_path=r'E:\DEV\PET_PROJECTS\selenium_parser\chromedriver\chromedriver.exe'  # noqa 
+#     )
+
+# путь веб драйвера для ноута
 service = Service(
-    executable_path=r'E:\DEV\PET_PROJECTS\selenium_parser\chromedriver\chromedriver.exe'  # noqa
+    executable_path=r'C:\dev\PET_PROJECT\selenium_parser\chromedriver\chromedriver.exe'  # noqa
     )
 
 # url
@@ -40,7 +47,7 @@ driver = webdriver.Chrome(service=service,
 
 # start
 try:
-    check_driver(driver)
+    # check_driver(driver)
     authorization_hh(driver, url, email, password)
     from_main_to_my_resumes(driver)
     from_my_resumes_to_recomended_vacations(driver)
@@ -48,8 +55,8 @@ try:
     pages_urls = get_pages_urls(driver)
     # print(pages_urls)
 
-    # collection = collecting_simple_info(pages_urls, driver)
-    collection = collecting_test_info(pages_urls, driver)
+    collection = collecting_simple_info(pages_urls, driver)
+    # collection = collecting_test_info(pages_urls, driver)
     result = get_vacancy_info(collection, driver)
     print()
 
