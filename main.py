c90d0372139d77d4ac0efd9da69c47a093284f5d
Solodnikov <solodnikov@yandex.ru>
2023-10-25 18:42:32 +0300

    Data updated

--- main.py
+++ main.py
@@ -33,14 +33,15 @@ options.add_argument('--disable-blink-features=AutomationControlled')
 # options.add_argument('--headless')
 
 # путь веб драйвера для пк
-service = Service(
-    executable_path=r'E:\DEV\PET_PROJECTS\selenium_parser\chromedriver\chromedriver.exe'  # noqa 
-    )
+# service = Service(
+#     executable_path=r'E:\DEV\PET_PROJECTS\selenium_parser\chromedriver\chromedriver.exe'  # noqa 
+#     )
 
 # путь веб драйвера для ноута
-# service = Service(
-#     executable_path=r'C:\dev\PET_PROJECT\selenium_parser\chromedriver\chromedriver.exe'  # noqa
-#     )
+service = Service(
+    executable_path=r'C:\dev\PET_PROJECT\selenium_parser\chromedriver\chromedriver.exe'  # noqa
+    )
+# TODO: завести путь к драйверу через PAthlib, что бы каждый раз не переключать
 
 # url
 url = 'https://hh.ru/account/login'
